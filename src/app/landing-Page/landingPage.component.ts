517277c34cd96439ef57e1090b39db075504a5eb
Theokleses <osahonschmolze222@gmail.com>
2025-01-20 22:32:57 +0100

    Added Image, background-image, job/name-text, and button

--- src/app/landing-Page/landingPage.component.ts
+++ src/app/landing-Page/landingPage.component.ts
@@ -10,8 +10,19 @@ import { NavbarComponent } from './navbar/navbar.component';
     template:`
     <section>
     <app-navbar></app-navbar>
+    <div class="first-sektion">
     <img src="./assets/img/file.png">
-
+    <div class="text-container">
+    <div class="name-job">
+    <span class="Iam">I am</span>
+    <p class="name">Osahon Schmolze</p>
+    <p class="job">FRONTEND DEVELOPER</p>
+    </div> 
+    <div class="center-button">
+    <button class="button">Lets's talk!</button>
+    </div> 
+  </div>
+  </div>
     </section>
     `,
     styleUrls:['./landingPage.component.scss']
