de3e73f57bb662d3e84f581b551cad61a12233ec
Theokleses <osahonschmolze222@gmail.com>
2025-01-24 00:37:43 +0100

    Added Portfolio and Reference Section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { LandPageComponent } from './landing-page/landingPage.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySKillsComponent } from './my-skills/my-skills.component';
 import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
+import { MyReferenceComponent } from './my-reference/my-reference.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandPageComponent, AboutMeComponent, MySKillsComponent, MyPortfolioComponent],
+  imports: [LandPageComponent, AboutMeComponent, MySKillsComponent, MyPortfolioComponent, MyReferenceComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/main-content/my-reference/my-reference.component.ts
+++ src/app/main-content/my-reference/my-reference.component.ts
@@ -5,8 +5,50 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './my-reference.component.html',
-  styleUrl: './my-reference.component.scss'
+  styleUrl: './my-reference.component.scss',
 })
 export class MyReferenceComponent {
+  images: string[] = [
+    'assets/img/first-reference.png',
+    'assets/img/second-reference.png',
+    'assets/img/third-reference.png',
+  ];
+  numberimages: string[] = [
+    'assets/img/firstimg-ref.png',
+    'assets/img/secondimg-ref.png',
+    'assets/img/thirdimg-ref.png',
+  ];
 
+  currentIndex: number = 0;
+  currentIndexNumber: number = 0;
+
+  // Pfeil-Bilder
+  leftArrowSrc = '/assets/img/arrow-left.png';
+  rightArrowSrc = '/assets/img/arrow-right.png';
+
+  // Hover-Bilder
+  leftArrowHover = '/assets/img/arrow-left-hover.png';
+  rightArrowHover = '/assets/img/arrow-right-hover.png';
+
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.currentIndexNumber =
+      (this.currentIndexNumber + 1) % this.numberimages.length;
+  }
+
+  previousImage(): void {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.currentIndexNumber =
+      (this.currentIndexNumber - 1 + this.numberimages.length) %
+      this.numberimages.length;
+  }
+
+  hoverArrow(direction: 'left' | 'right', isHovering: boolean) {
+    if (direction === 'left') {
+      this.leftArrowSrc = isHovering ? this.leftArrowHover : '/assets/img/arrow-left.png';
+    } else if (direction === 'right') {
+      this.rightArrowSrc = isHovering ? this.rightArrowHover : '/assets/img/arrow-right.png';
+    }
+  }
 }
