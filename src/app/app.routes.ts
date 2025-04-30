1beee70cb47a4b2db5b022448d42a00a8bf617d5
Theokleses <osahonschmolze222@gmail.com>
2025-04-30 21:29:27 +0200

    fix(routing): adjusted routes to navigate back to landing page and eliminate console issues
    Refactored routing setup by replacing ambiguous root path ('') with explicit 'landing' route for MainContentComponent. Updated routerLink targets accordingly. This resolves navigation bugs and prevents console warnings when returning from 'imprint' or 'legal-notice' views.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,21 +5,13 @@ import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintLayoutComponent } from './imprint-layout/imprint-layout.component';
 
-
-// export const routes: Routes = [
-//     {path: 'imprint', component: ImprintComponent},
-//     {path: 'legal-notice', component: LegalNoticeComponent},
-//     {path: '', component: MainContentComponent},
-//     {path: 'imprint', component: ImprintLayoutComponent},
-// ];
-
 export const routes: Routes = [
-    { 
-      path: 'imprint', component: ImprintLayoutComponent,
-      children: [
-        { path: '', component: ImprintComponent } 
-      ]
-    },
-    { path: 'legal-notice', component: LegalNoticeComponent},
-    { path: '', component: MainContentComponent }
-  ];
+  { path: 'imprint', component: ImprintLayoutComponent,
+    children: [
+      { path: '', component: ImprintComponent }
+    ]
+  },
+  { path: 'legal-notice', component: LegalNoticeComponent },
+  { path: 'landing', component: MainContentComponent },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' }
+];
